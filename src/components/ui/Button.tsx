2c8f05a1133d0aad5304f6a90c333db3ab810fc5
Tere <usuario@ejemplo.com>
2026-04-24 20:50:49 +0200

    Fix: removed unused radix-ui import in Button.tsx

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,5 +1,4 @@
 import * as React from "react"
-import { slot } from "@radix-ui/react-slot" // Wait, I didn't install radix. I'll use simple button.
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
